2e0426a2dbf75b1fe76da8c559eba19bdfb79948
Cameron <c.bryce996@gmail.com>
2022-09-25 16:38:26 +0100

    Initial project scaffold, ASP.NET Core MVC App with ElectronNET for desktop development

---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace project_cbryce996.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
