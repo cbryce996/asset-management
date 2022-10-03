5d12ccea3d08d63d6739dcc5d04e70cb30b735f1
Cameron <c.bryce996@gmail.com>
2022-10-03 12:59:32 +0100

    jQuery fix

--- src/wwwroot/js/script.js
+++ src/wwwroot/js/script.js
@@ -4,13 +4,9 @@
 * Author: Alexis Luna
 * Website: https://github.com/alexis-luna/bootstrap-simple-admin-template
 -------------------------------------------------------------------*/
-alert('Testing Simple Admin Template');
-
 $(document).ready(function () {
-    alert('Loaded JQuery');
     // Toggle sidebar on Menu button click
     $('#sidebarCollapse').click(function() {
-        alert('Click');
         $('#sidebar').toggleClass('active');
         $('#body').toggleClass('active');
     });
